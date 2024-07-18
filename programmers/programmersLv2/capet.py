2fc77843e21c3e0fc38e7e5690feee0707a10930
Leejeongin <jeonginlee72@gmail.com>
2024-07-18 22:22:23 +0900

    programmersLv2 카펫 clear

--- programmers/programmersLv2/capet.py
+++ programmers/programmersLv2/capet.py
@@ -1,8 +1,11 @@
 def solution(brown, yellow):
-    sumCR = brown/2 + 2
-    mulCR = yellow -4 + 4*sumCR
-    
-    col = murCR // 2
-    while True:
-        if mulCR
-        
+    sumCR = brown / 2 + 2
+    mulCR = yellow - 4 + 2 * sumCR
+
+    col = mulCR // 2
+    while col > 0:
+        if mulCR % col == 0:
+            row = mulCR / col
+            if sumCR == (col + row):
+                return [col, row]
+        col -= 1
