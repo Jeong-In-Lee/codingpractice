208f29ba3109cf014b40c8c6f2f62a27e80f403b
DESKTOP-O9A3R98\김숙희 <jeonginlee72@gmail.com>
2024-09-05 21:30:26 +0900

    baekjoon sayMid 1655 clear

---
+++ baekjoon/sayMid1655.py
@@ -0,0 +1,50 @@
+
+#시간 초과 (아무래도 O(n^2) 이라 그런 듯)
+import heapq
+import copy
+
+def firstTry():
+    N = int(input())
+    heap = []
+    for i in range(N):
+        heapq.heappush(heap, int(input()))
+        temp = copy.copy(heap)
+        if i==0:
+            print(heap[0])
+        elif i%2==0:
+            n=i//2-1
+            while n>0:
+                n-=1
+                heapq.heappop(temp)
+            print(temp[0])
+        else:
+            n=i//2
+            while n>0:
+                n-=1
+                heapq.heappop(temp)
+            print(temp[0])
+
+
+# nlog n 인데 이게 시간초과라니..
+def secondTry():
+    N = int(input())
+    leftHeap = [] #최대힙
+    rightHeap = [] #최소힙
+    mid = 0
+    for i in range(N):
+        num = int(input())
+        if i==0:
+            mid = num
+            print(mid)
+        elif num>=mid:
+            heapq.heappush(rightHeap, num)
+            if len(rightHeap)-len(leftHeap)>=2:
+                heapq.heappush(leftHeap, -1*mid)
+                mid = heapq.heappop(rightHeap)
+            print(mid)
+        else:
+            heapq.heappush(leftHeap, -1*num)
+            if len(leftHeap)-len(rightHeap)>=2:
+                heapq.heappush(rightHeap, mid)
+                mid = -1*heapq.heappop(leftHeap)
+            print(mid)
